292a16a6701fc07deb899c297eb0bf87e1bbaca5
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2012-09-12 00:12:01 +0530

    init repo. basic stuff done.

---
+++ README.rst
@@ -0,0 +1,8 @@
+====
+Marg
+====
+
+About
+-----
+Marg is an attempt to create an easy-to-use and extensible request router for
+PHP programmers.
---
+++ marg/marg.php
@@ -0,0 +1,39 @@
+<?php
+
+include 'includes/Request.php';
+
+$request = new Request();
+
+class Marg {
+    public function __construct() {}
+
+    public static function run($routes) {
+        global $request;
+        $controller_name = '';
+        $matches = array();
+        foreach ($routes as $pattern => $controller) {
+            if (preg_match($pattern, $request->uri, $match)) {
+                $controller_name = $controller;
+                $matches = $match;
+                break;
+            }
+        }
+
+        if (class_exists($controller_name)) {
+            $controller = new $controller_name($matches);
+
+            $method = strtolower($request->verb);
+            if (method_exists($controller, $method)) {
+                call_user_func(array($controller, $method), $matches);
+            }
+        } elseif (function_exists($controller_name)) {
+            call_user_func($controller_name, $matches);
+        } else {
+            ;
+            // raise http exception
+        }
+    }
+
+};
+
+?>
