6557fbbc076e91f3c9f3e720f27528aafdf2bdd2
Vaidik Kapoor <kapoor.vaidik@gmail.com>
2012-09-24 04:03:18 +0530

    before and after requests, controllers by verb limiting implemented

--- marg/marg.php
+++ marg/marg.php
@@ -5,9 +5,22 @@ include 'includes/Request.php';
 $request = new Request();
 
 class Marg {
-    public function __construct() {}
+    private static $setUp;
+    private static $tearDown;
+
+    public static function addSetUp($func) {
+        self::$setUp = $func;
+    }
+
+    public static function addTearDown($func) {
+        self::$tearDown = $func;
+    }
 
     public static function run($routes) {
+        if (isset(self::$setUp)) {
+            call_user_func(self::$setUp);
+        }
+
         global $request;
         $controller_name = '';
         $matches = array();
@@ -19,21 +32,62 @@ class Marg {
             }
         }
 
-        if (class_exists($controller_name)) {
+        if (is_array($controller_name)) {
+            // By default GET
+            $methods = array('GET');
+
+            if (isset($controller_name['controller'])) {
+                if (isset($controller_name['methods'])) {
+                    $methods = $controller_name['methods'];
+                }
+                $controller_name = $controller_name['controller'];
+            } else {
+                if (isset($controller_name[1]) && is_array($controller_name[1])) {
+                    $methods = $controller_name[1];
+                }
+                $controller_name = $controller_name[0];
+            }
+
+            if (in_array(strtoupper($request->verb), $methods)) {
+                call_user_func_array($controller_name, $matches);
+            }
+        } elseif (class_exists($controller_name)) {
             $controller = new $controller_name($matches);
 
+            call_user_func_if_exists(array($controller, 'setUp'));
             $method = strtolower($request->verb);
             if (method_exists($controller, $method)) {
-                call_user_func(array($controller, $method), $matches);
+                call_user_func_array(array($controller, $method), $matches);
             }
+            call_user_func_if_exists(array($controller, 'tearDown'));
         } elseif (function_exists($controller_name)) {
-            call_user_func($controller_name, $matches);
+            call_user_func_array($controller_name, $matches);
         } else {
-            throw new Exception;
-            // raise http exception
+            raise('404');
+        }
+
+        if (isset(self::$tearDown)) {
+            call_user_func(self::$tearDown);
         }
     }
-
 };
+
+function raise($code) {
+    $args = func_get_args();
+    $args = array_slice($args, 1);
+    call_user_func_if_exists('raise_' . $code, $args);
+}
+
+function call_user_func_if_exists($func, $args = array()) {
+    $retval = null;
+    if (is_array($func)) {
+        if (method_exists($func[0], $func[1])) {
+            $retval = call_user_func_array(array($func[0], $func[1]), $args);
+        }
+    } else {
+        $retval = call_user_func_array($func, $args);
+    }
+    return $retval;
+}
 
 ?>
